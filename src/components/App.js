1a727eac862b7216ddf87eadfae83e6d80efbbf0
Gabriel Sargent <gabe2sargent@gmail.com>
2023-08-02 01:20:43 -0500

    reactbaslab

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,14 @@ function Home() {
     </div>
   );
 }
+function About(){
+  return(
+    <div id="about">
+      <h2>"About"</h2>
+    </div>
+
+  );
+}
 
 {/* write an <About> component here */}
 
@@ -23,8 +31,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
